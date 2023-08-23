24f2adc1038bce56e08001379e579721f0dc6872
Hemachandran S <85756930+HemachandranS@users.noreply.github.com>
2023-08-23 22:07:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,31 +2,32 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/calculator/greeting', methods=['GET'])
+@app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Hello world!", 200
+    return "Hello world!"
 
-@app.route('/calculator/add', methods=['POST'])
+@app.route("/calculator/add", methods=['POST'])
 def add():
     try:
         data = request.json
         first = data['first']
         second = data['second']
         result = first + second
-        return jsonify({"result": result}), 200
-    except KeyError:
-        return "Invalid data format", 400
+        return jsonify({'result': result}), 200
+    except Exception as e:
+        return jsonify({'error': 'Invalid input'}), 400
 
-@app.route('/calculator/subtract', methods=['POST'])
+@app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     try:
         data = request.json
         first = data['first']
         second = data['second']
         result = first - second
-        return jsonify({"result": result}), 200
-    except KeyError:
-        return "Invalid data format", 400
+        return jsonify({'result': result}), 200
+    except Exception as e:
+        return jsonify({'error': 'Invalid input'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, host='0.0.0.0')
+
