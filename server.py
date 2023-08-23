f4ed72a16eca5e82e4158171b157424012e24da2
Hemachandran S <85756930+HemachandranS@users.noreply.github.com>
2023-08-23 22:02:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,32 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return "Hello world!"
- 
-@app.route("/calculator/add", methods=['POST'])
+    return "Hello world!", 200
+
+@app.route('/calculator/add', methods=['POST'])
 def add():
-    data = request.get_json()
-    first = data.get('first', 0)
-    second = data.get('second', 0)
-    result = first + second
-    return jsonify({"result": result}), 200
+    try:
+        data = request.json
+        first = data['first']
+        second = data['second']
+        result = first + second
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return "Invalid data format", 400
 
-@app.route("/calculator/subtract", methods=['POST'])
+@app.route('/calculator/subtract', methods=['POST'])
 def subtract():
-    data = request.get_json()
-    first = data.get('first', 0)
-    second = data.get('second', 0)
-    result = first - second
-    return jsonify({"result": result}), 200
+    try:
+        data = request.json
+        first = data['first']
+        second = data['second']
+        result = first - second
+        return jsonify({"result": result}), 200
+    except KeyError:
+        return "Invalid data format", 400
 
-if _name_ == '_main_':
-    app.run(port=8080, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
